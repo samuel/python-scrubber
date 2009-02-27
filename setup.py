e01802b3aaf36b92d3a10f8630c7535530fdc64e
Samuel Stauffer <samuel@descolada.com>
2009-02-27 14:47:47 -0800

    Version now 1.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='scrubber',
-      version='1.1',
+      version='1.1.1',
       description='A whitelisting HTML scrubber',
       url='http://github.com/samuel/python-scrubber/tree/master',
       packages=['scrubber'],
