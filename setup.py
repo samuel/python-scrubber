c1900c93c3adad611addd8731f18418966995ec7
Samuel Stauffer <samuel@descolada.com>
2009-04-22 14:12:59 -0700

    Changed description: scrubber -> sanitizer

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 setup(
     name = 'scrubber',
     version = '1.3.4',
-    description = 'A whitelisting HTML scrubber',
+    description = 'A whitelisting HTML sanitizer',
     author = 'Samuel Stauffer',
     author_email = 'samuel@descolada.com',
     url = 'http://github.com/samuel/python-scrubber/tree/master',
