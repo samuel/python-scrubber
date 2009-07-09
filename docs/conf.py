736e45530bc653ec4952d06a8a5bc59d86d34ed3
Samuel Stauffer <samuel@descolada.com>
2009-07-08 17:04:01 -0700

    Added github.tools.sphinx extension to sphinx config to get rid of undescores in directory names as they don't work as expected in github pages.

--- docs/conf.py
+++ docs/conf.py
@@ -23,6 +23,12 @@ import sys, os
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.todo']
+try:
+    from github.tools import sphinx
+except ImportError:
+    pass
+else:
+    extensions.append('github.tools.sphinx')
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
